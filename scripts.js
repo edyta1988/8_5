734a96fd705fa47876398b9c8d48e20c5acaaf3d
Edyta Zawada <edyta.zawada88@gmail.com>
2017-08-06 10:16:12 +0200

    project start

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var a = prompt('Enter base');
+	h = prompt('Enter height');
+	triangleArea = a*h/2;
+
+console.log(triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
